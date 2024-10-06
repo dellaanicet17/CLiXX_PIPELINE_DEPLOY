d341318ffb0db0e3b9e0607a7806d4d7fabc7321
dellaanicet17 <dellaanicet17@gmail.com>
2024-10-06 03:30:24 -0400

    adding files

--- delete_db_instance.py
+++ delete_db_instance.py
@@ -12,7 +12,6 @@ credentials=assumed_role_object['Credentials']
 print(credentials)
 
 # Create an RDS client
-#rds_client=boto3.client('rds',aws_access_key_id=credentials['AccessKeyId'],aws_secret_access_key=credentials['SecretAccessKey'],aws_session_token=credentials['SessionToken'])
 rds_client=boto3.client('rds',aws_access_key_id=credentials['AccessKeyId'],aws_secret_access_key=credentials['SecretAccessKey'],aws_session_token=credentials['SessionToken'])
 # Delete the DB instance
 response = rds_client.delete_db_instance(
