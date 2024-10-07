e6b75ecb17e82102f1b972819faafe8ad1a866bf
dellaanicet17 <dellaanicet17@gmail.com>
2024-10-07 17:33:59 -0400

    adding files

--- deploy_clixx.py
+++ deploy_clixx.py
@@ -1,13 +1,42 @@
 #!/usr/bin/env python3
 import boto3,botocore
 
-AWS_REGION = "us-east-1"
-KEY_PAIR_NAME = 'stack_devops_kp'
-AMI_ID = 'ami-00f251754ac5da7f0' # Amazon Linux 2
-SUBNET_ID = 'subnet-0e9f4974af6be42ae' 
-SECURITY_GROUP_ID = 'sg-024f0157b123d6a8c'
-INSTANCE_PROFILE = 'CLiXX'
-USER_DATA = '''#!/bin/bash -xe
+sts_client=boto3.client('sts')
+#Calling the assume_role function
+assumed_role_object=sts_client.assume_role(RoleArn='arn:aws:iam::043309319757:role/Engineer', RoleSessionName='mysession')
+credentials=assumed_role_object['Credentials']
+print(credentials)
+
+# Variables
+db_instance_identifier = 'wordpressdbclixx'
+db_snapshot_identifier = 'arn:aws:rds:us-east-1:577701061234:snapshot:wordpressdbclixx-ecs-snapshot'
+db_instance_class = 'db.m6gd.large'  # Example instance class
+availabilityzone = 'us-east-1a'
+security_group_id = 'sg-024f0157b123d6a8c'
+
+# Create RDS client
+rds_client=boto3.client('rds',aws_access_key_id=credentials['AccessKeyId'],aws_secret_access_key=credentials['SecretAccessKey'],aws_session_token=credentials['SessionToken'])
+
+# Restore DB instance from snapshot
+response = rds_client.restore_db_instance_from_db_snapshot(
+    DBInstanceIdentifier=db_instance_identifier,
+    DBSnapshotIdentifier=db_snapshot_identifier,
+    DBInstanceClass=db_instance_class,
+    VpcSecurityGroupIds=security_group_id,
+    AvailabilityZone=availabilityzone,
+    MultiAZ=False,
+    PubliclyAccessible=True
+)
+
+############################################################
+
+# Variables
+aws_region = "us-east-1"
+key_pair_name = 'stack_devops_kp'
+ami_id = 'ami-00f251754ac5da7f0' # Amazon Linux 2
+subnet_id = 'subnet-0e9f4974af6be42ae' 
+security_group_id = 'sg-024f0157b123d6a8c'
+user_data = '''#!/bin/bash -xe
  # Variables - Update these as needed
 CLiXX_GIT_REPO_URL="https://github.com/stackitgit/CliXX_Retail_Repository.git"
 WordPress_DB_NAME="wordpressdb" 
@@ -71,27 +100,26 @@ PUBLIC_IPV4=$(curl -s -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.2
 
 # Run the following CliXX_Deployment_Update_Statements to refresh the webpage to update the IP address 
 mysql -u ${WordPress_DB_USER} -p${WordPress_DB_PASS} -h ${WordPress_DB_HOST} -D ${WordPress_DB_NAME} <<EOF
-UPDATE wp_options SET option_value = "${PUBLIC_IPV4}" WHERE option_value = "CliXX-APP-NLB-a7ece55c49d173cc.elb.us-east-1.amazonaws.com";
-UPDATE wp_options SET option_value = "${PUBLIC_IPV4}" WHERE option_value LIKE 'http%';
+    UPDATE wp_options SET option_value = "${PUBLIC_IPV4}" WHERE option_name = "home";
+    UPDATE wp_options SET option_value = "${PUBLIC_IPV4}" WHERE option_name = "siteurl";
+    UPDATE wp_options SET option_value = "${PUBLIC_IPV4}" WHERE option_name = "ping_sites";
+    UPDATE wp_options SET option_value = "${PUBLIC_IPV4}" WHERE option_name = "open_shop_header_retina_logo";
+#EOF
 '''
 
-sts_client=boto3.client('sts')
-#Calling the assume_role function
-assumed_role_object=sts_client.assume_role(RoleArn='arn:aws:iam::043309319757:role/Engineer', RoleSessionName='mysession')
-credentials=assumed_role_object['Credentials']
-print(credentials)
+# Create EC2 resource
+ec2_resource = boto3.resource('ec2',region_name=aws_region,aws_access_key_id=credentials['AccessKeyId'],aws_secret_access_key=credentials['SecretAccessKey'],aws_session_token=credentials['SessionToken'])
 
-EC2_RESOURCE = boto3.resource('ec2',region_name=AWS_REGION,aws_access_key_id=credentials['AccessKeyId'],aws_secret_access_key=credentials['SecretAccessKey'],aws_session_token=credentials['SessionToken'])
-EC2_CLIENT = boto3.client('ec2',region_name=AWS_REGION,aws_access_key_id=credentials['AccessKeyId'],aws_secret_access_key=credentials['SecretAccessKey'],aws_session_token=credentials['SessionToken'])
-instances = EC2_RESOURCE.create_instances(
+# Create New Instance
+instances = ec2_resource.create_instances(
     MinCount = 1,
     MaxCount = 1,
-    ImageId=AMI_ID,
+    ImageId=ami_id,
     InstanceType='t2.micro',
-    KeyName=KEY_PAIR_NAME,
-    SecurityGroupIds = [SECURITY_GROUP_ID],
-    SubnetId=SUBNET_ID,
-    UserData=USER_DATA,
+    KeyName=key_pair_name,
+    SecurityGroupIds = [security_group_id],
+    SubnetId=subnet_id,
+    UserData=user_data,
     TagSpecifications=[
         {
             'ResourceType': 'instance',
