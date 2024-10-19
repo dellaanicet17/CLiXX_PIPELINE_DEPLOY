035e78302a94847537489b1e774d5ee0d2426e3d
dellaanicet17 <dellaanicet17@gmail.com>
2024-10-19 03:08:06 -0400

    add files

--- deploy-clixx-cust-vpc.py
+++ deploy-clixx-cust-vpc.py
@@ -310,8 +310,7 @@ while True:
         time.sleep(10)
 
 # After ensuring the file system is available, create the mount targets in the private subnets
-private_subnet_ids = [private_subnet_1['SubnetId'], private_subnet_2['SubnetId']]
-
+private_subnet_ids = [private_subnet_1['SubnetIdentifier'], private_subnet_2['SubnetIdentifier']]
 for private_subnet_id in private_subnet_ids:
     # Check if mount target already exists for the subnet
     mount_targets_response = efs_client.describe_mount_targets(
